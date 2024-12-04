2dba60cefc9846689e4f861febb593b2a44c709d
zhangxinhui <zhang.xinhui@cesgroup.com.cn>
2024-12-04 15:39:29 +0800

    feat: 配置clerk登录方式/新增middleware登录校验;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: "kiwi",
@@ -13,8 +20,18 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="zh-cn" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="zh-cn" className={`${GeistSans.variable}`}>
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
