65a20f058a81f6c958259c58b34ecd7e9fd0193b
zhangxinhui <xinhui.zhang@wuitu.com>
2025-01-15 17:51:10 +0800

    feat: prettier配置更改适配;

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,14 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
-    tabWidth: 4, // 缩进4个字符
-    printWidth: 120, //超过多少换行
-    singleQuote: true, //字符串是否使用单引号，默认为false，使用双引号
-    semi: true, //行位是否使用分号，默认为true
-    trailingComma: 'all', //是否使用尾逗号，有三个可选值"<none|es5|all>"
-    arrowParens: 'avoid', // x => x
-    jsxBracketSameLine: true,
-    requirePragma: false, //顶部注释
-    bracketSpacing: false, // 对象中的空格 默认true
-    htmlWhitespaceSensitivity: 'ignore',
-    plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  plugins: ['prettier-plugin-tailwindcss'],
 };
