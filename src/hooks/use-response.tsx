d1ec98b6ccbc2053c768894cc6c1c994c1fc8f75
zhangxinhui <xinhui.zhang@wuitu.com>
2025-01-17 15:31:27 +0800

    feat: 新增serverRes/clientRes分别用以公共处理服务端响应和客户端响应;

---
+++ src/hooks/use-response.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { type ServerResponse } from '@/actions/middleware/response';
+import { useToast } from './use-toast';
+import { useCallback } from 'react';
+
+export const useHandleClientResponse = <T,>() => {
+  const { toast } = useToast();
+
+  return useCallback(
+    async (
+      promise: Promise<ServerResponse<T>>,
+      options?: {
+        showSuccessMessage?: boolean;
+        successMessage?: string;
+        showErrorMessage?: boolean;
+      },
+    ): Promise<T | null> => {
+      const { showSuccessMessage = false, showErrorMessage = true, successMessage } = options || {};
+
+      try {
+        const response = await promise;
+        if (response.success) {
+          if (showSuccessMessage) {
+            toast({
+              title: successMessage ?? response.message,
+            });
+          }
+          return response.data ?? null;
+        } else {
+          if (showErrorMessage) {
+            toast({
+              title: response.message,
+              variant: 'destructive',
+            });
+          }
+          return null;
+        }
+      } catch (error) {
+        toast({
+          title: '请求失败，请稍后重试',
+          variant: 'destructive',
+        });
+        return null;
+      }
+    },
+    [toast],
+  );
+};
