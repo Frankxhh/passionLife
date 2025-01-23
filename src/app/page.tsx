5e1d644041d5d23fe13c85b31401aad7a6380b85
zhangxinhui <xinhui.zhang@wuitu.com>
2025-01-23 16:09:00 +0800

    feat: 调整首页结构-优化骨架屏展示效果;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import WeightTrendsChart from '@/components/home/WeightTrendsChart';
 import TargetCard from '@/components/home/TargetCard';
 import { getUserInfoAction, getUserWeekTrendAction } from '@/actions/user';
 import { getUserTargetAction } from '@/actions/userTarget';
+import FallBack from '@/components/home/FallBack';
 
 function Fallback() {
   return <Skeleton className="h-[180px] w-full" />;
@@ -30,37 +31,39 @@ const targetList: TargetListItem[] = [
   },
 ];
 
-export default async function ProfilePage() {
+const ProfilePage = async () => {
   const userInfo = await getUserInfoAction();
   const userWeekTrend = await getUserWeekTrendAction();
   const userTarget = await getUserTargetAction();
   return (
     <div className={'h-full w-full p-4'}>
-      <Suspense fallback={<Fallback />}>
-        <Card className={'mb-4 bg-gradient-to-b from-[#69B1FF] to-[#1677ff] text-white'}>
-          <CardContent className={'px-4 pb-4 pt-2'}>
-            <PhysicalData userInfo={userInfo.data ?? null} message={userInfo.message} />
-          </CardContent>
-        </Card>
-      </Suspense>
-      <Suspense fallback={<Fallback />}>
-        <Card className={'mb-4'}>
-          <CardHeader className={'px-4 py-4 pb-0'}>
-            <CardTitle className={'flex justify-between'}>
-              <span>趋势</span>
-              <span>周报</span>
-            </CardTitle>
-          </CardHeader>
-          <CardContent className={'p-4 py-0'}>
-            <WeightTrendsChart userWeekTrend={userWeekTrend.data ?? null} message={userWeekTrend.message} />
-          </CardContent>
-        </Card>
-      </Suspense>
-      <Suspense fallback={<Fallback />}>
-        {targetList.map(item => (
-          <TargetCard key={item.title} targetItem={item} userTarget={userTarget.data ?? null} />
-        ))}
-      </Suspense>
+      <Card className={'mb-4 bg-gradient-to-b from-[#69B1FF] to-[#1677ff] text-white'}>
+        <CardContent className={'px-4 pb-4 pt-2'}>
+          <PhysicalData userInfo={userInfo.data ?? null} message={userInfo.message} />
+        </CardContent>
+      </Card>
+      <Card className={'mb-4'}>
+        <CardHeader className={'px-4 py-4 pb-0'}>
+          <CardTitle className={'flex justify-between'}>
+            <span>趋势</span>
+            <span>周报</span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent className={'p-4 py-0'}>
+          <WeightTrendsChart userWeekTrend={userWeekTrend.data ?? null} message={userWeekTrend.message} />
+        </CardContent>
+      </Card>
+      {targetList.map(item => (
+        <TargetCard key={item.title} targetItem={item} userTarget={userTarget.data ?? null} />
+      ))}
     </div>
+  );
+};
+
+export default function Page() {
+  return (
+    <Suspense fallback={<FallBack />}>
+      <ProfilePage />
+    </Suspense>
   );
 }
---
+++ src/components/home/FallBack.tsx
@@ -0,0 +1,13 @@
+import { Skeleton } from '../ui/skeleton';
+
+const FallBack = () => {
+  return (
+    <div className="h-full w-full space-y-4 p-4">
+      <Skeleton className="h-48 w-full" />
+      <Skeleton className="h-44 w-full" />
+      <Skeleton className="h-40 w-full" />
+      <Skeleton className="h-40 w-full" />
+    </div>
+  );
+};
+export default FallBack;
